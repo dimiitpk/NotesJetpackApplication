792c08b694c6cd78488a2ed34b2821dc8c57cf67
Dimi <acika993.ad@gmail.com>
2020-12-24 18:17:06 +0100

    Commit

--- app/src/main/java/com/dimi/advnotes/data/interactors/InsertOrUpdateNoteUseCase.kt
+++ app/src/main/java/com/dimi/advnotes/data/interactors/InsertOrUpdateNoteUseCase.kt
@@ -7,9 +7,6 @@ class InsertOrUpdateNoteUseCase(
     private val cacheDataSource: CacheDataSource
 ) {
     suspend operator fun invoke(note: Note): Long {
-//        return withContext(Dispatchers.IO) {
-//            cacheDataSource.insertOrUpdateIfExist(note)
-//        }
         return cacheDataSource.insertOrUpdateIfExist(note)
     }
 }
